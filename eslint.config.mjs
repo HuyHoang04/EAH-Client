5866652098cbafc6770c586e1673ebd20b7eb9f5
La Huy Hoang <127657744+HuyHoang04@users.noreply.github.com>
2025-10-14 11:46:25 +0700

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -21,8 +21,10 @@ const eslintConfig = [
     ],
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-unused-vars": "off", 
-      "react/no-unescaped-entities": "off", 
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "react/no-unescaped-entities": "off",
+      "react-hooks/exhaustive-deps": "off"
     },
   },
 ];
