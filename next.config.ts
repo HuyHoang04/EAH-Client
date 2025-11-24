3d405172a05ad63dd2ba5cf64350a3b6c9723c6f
La Huy Hoang <127657744+HuyHoang04@users.noreply.github.com>
2025-11-24 14:07:25 +0700

    Update Next.js, adjust config, and move favicon
    Upgraded Next.js and @types/node to latest versions in package.json. Refactored next.config.ts to improve build and cache settings. Moved favicon.ico from src/app to public and removed custom icons metadata from layout.tsx.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  ignoreDuringBuilds: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  webpack: (config) => {
+    config.resolve.symlinks = false;
+    config.cache = false; // BẮT BUỘC phải tắt cache
+    
+    // Tắt hoàn toàn snapshot
+    if (config.snapshot) {
+      config.snapshot.managedPaths = [];
+      config.snapshot.immutablePaths = [];
+      config.snapshot.buildDependencies = undefined;
+    }
+    
+    return config;
+  },
 };
 
 export default nextConfig;
